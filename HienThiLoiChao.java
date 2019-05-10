33db3c4dee691df99a79e222a1c689fc982b4611
BTienDung <tiendung0899@gmail.com>
2019-05-10 18:19:02 +0700

    nb

---
+++ HienThiLoiChao.java
@@ -0,0 +1,11 @@
+package season1.HienThiLoiChao;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println("Hello " +str);
+    }
+}
